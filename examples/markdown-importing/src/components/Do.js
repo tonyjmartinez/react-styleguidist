6e7a58c9b5fc0c4f2237b8febabe1df6f71f6ba5
tonyjmartinez <ajosephmartinez@gmail.com>
2019-07-24 14:14:25 -0700

    .

--- examples/markdown-importing/src/components/Do.js
+++ examples/markdown-importing/src/components/Do.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Do = props => <div className="Do">{props.children}</div>;
+const Do = ({ children }) => <div className="Do">{children}</div>;
 
 export default Do;
