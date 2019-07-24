6bd673161e80fd525c8be001be63026d04f860a3
tonyjmartinez <ajosephmartinez@gmail.com>
2019-07-24 14:13:48 -0700

    Helping user with markdown import problem

---
+++ examples/markdown-importing/src/components/Do.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Do = props => <div className="Do">{props.children}</div>;
+
+export default Do;
---
+++ examples/markdown-importing/src/components/Readme.md
@@ -0,0 +1,6 @@
+Do Markdown
+
+```jsx
+import Do from 'myComponents/Do'
+;<Do>You should do this thing.</Do>
+```
